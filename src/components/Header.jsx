841446c0a0922a6e713af78292f493b1f1b79c1c
shadon016 <shadon016@gmail.com>
2024-03-24 12:47:29 +0600

    setup header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GiMoon } from "react-icons/gi";
+import { IoIosNotifications } from "react-icons/io";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+    <nav className="flex px-6 justify-between items-center h-20 md:px-12">
+    <div>
+        <p className="uppercase font-bold">cine hall</p>
+    </div>
+    <ul className="flex gap-3">
+        <li><IoIosNotifications /></li>
+        <li><GiMoon /></li>
+        <li><AiOutlineShoppingCart /></li>
+    </ul>
+   </nav>
+    </>
   )
 }
