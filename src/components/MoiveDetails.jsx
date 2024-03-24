d09aa9f2e0e3ae04074966a391354bf3d2b32704
shadon016 <shadon016@gmail.com>
2024-03-24 12:23:07 +0600

    creating required components

---
+++ src/components/MoiveDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MoiveDetails() {
+  return (
+    <div>MoiveDetails</div>
+  )
+}
---
+++ src/components/MoiveList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MoiveList() {
+  return (
+    <div>MoiveList</div>
+  )
+}
